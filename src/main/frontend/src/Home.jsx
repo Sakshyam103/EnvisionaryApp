6910c7e3d8a34346771280b5ed9a7807dfdbd532
Kritika Parajuli <124526898+kparajul@users.noreply.github.com>
2023-11-26 21:40:43 -0500

    button clicked

--- src/main/frontend/src/Home.jsx
+++ src/main/frontend/src/Home.jsx
@@ -7,14 +7,34 @@ import NavigationBar from './components/NavigationBar';
 function Home() {
   const [notification, setNotification] = useState('');
 
-  const handleButtonClick = (buttonName) => {
-    setNotification(`You clicked ${buttonName} button!`);
+  const handleMakePredictions = () => {
+    // Logic for 'Make Prediction' button
+    console.log("Make Prediction clicked");
+  };
+
+  const handleViewPredictions = () => {
+    // Logic for 'View Prediction' button
+    console.log("View Prediction clicked");
+  };
+
+  const handleViewStatistics = () => {
+    // Logic for 'View Statistics' button
+    console.log("View Statistics clicked");
+  };
+
+  const handleNotifications = () => {
+    // Logic for 'Notifications' button
+    console.log("Notifications clicked");
   };
 
   return (
     
     <div className="home-container">
-      <NavigationBar handleButtonClick={handleButtonClick}
+      <NavigationBar 
+      handleMakePredictions={handleMakePredictions}
+      handleViewPredictions={handleViewPredictions}
+      handleViewStatistics={handleViewStatistics}
+      handleNotifications={handleNotifications}
       
       />
       {notification && (
--- src/main/frontend/src/components/NavigationBar.jsx
+++ src/main/frontend/src/components/NavigationBar.jsx
@@ -4,17 +4,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './NavigationBar.css';
 //import myLogo from './assets/myLogo.png';
-function NavigationBar({ handleButtonClick }) {
+function NavigationBar({ handleMakePredictions, handleViewPredictions, handleViewStatistics, handleNotifications }) {
   return (
     <nav className="navbar">
       <ul>
       {/* <div className="logo-container">
         <img src={myLogo} alt="logo" className="My logo" />
       </div> */}
-        <li><button className="nav-button" onClick={() => handleButtonClick('MakePrediction')}>Make Prediction</button></li>
-        <li><button className="nav-button" onClick={() => handleButtonClick('ViewPrediction')}>View Prediction</button></li>
-        <li><button className="nav-button" onClick={() => handleButtonClick('ViewStatistics')}>View Statistics</button></li>
-        <li><button className="nav-button" onClick={() => handleButtonClick('Notifications')}>Notifications</button></li>
+      <li><button className="nav-button" onClick={handleMakePredictions}>Make Predictions</button></li>
+        <li><button className="nav-button" onClick={handleViewPredictions}>View Predictions</button></li>
+        <li><button className="nav-button" onClick={handleViewStatistics}>View Statistics</button></li>
+        <li><button className="nav-button" onClick={handleNotifications}>Notifications</button></li>
         {/* Add more navigation links as needed */}
       </ul>
     </nav>
