bac17155d3f6805f347ad8f085a2d7041ae1bdb5
Brandon LaPointe <126349214+Brandomon@users.noreply.github.com>
2023-12-02 01:38:44 -0500

    Fixed imports

--- src/main/java/backend/Notifications/NotificationUpdater.java
+++ src/main/java/backend/Notifications/NotificationUpdater.java
@@ -3,8 +3,8 @@ package backend.Notifications;
 import backend.CelestialBodyPredictions.CelestialBodyPrediction;
 import backend.FootballMatchPredictions.FootballMatchPrediction;
 import backend.UserInfo.MongoDBEnvisionaryUsers;
-
 import backend.WeatherPredictions.WeatherPrediction;
+
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import java.io.File;
