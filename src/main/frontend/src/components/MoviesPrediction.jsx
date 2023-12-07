f203145495eca3702d5052e4ab527eb539f7913b
Kritika Parajuli <124526898+kparajul@users.noreply.github.com>
2023-12-07 10:52:18 -0500

    Movies

--- src/main/frontend/src/components/MoviesPrediction.jsx
+++ src/main/frontend/src/components/MoviesPrediction.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+//import {useNavigate} from "react-router-dom";
 
 function MoviesPrediction() {
   const [movie, setMovie] = useState('');
   const [releaseYear, setReleaseYear] = useState('');
     const [error, setError] = useState('');
+    //const navigate = useNavigate();
   
   const years = Array.from({ length: 74 }, (_, index) => (2023 - index).toString());
 
@@ -29,6 +31,39 @@ function MoviesPrediction() {
         })
 
        ;
+
+          //
+          // console.log("Prediction resolving");
+          // fetch("http://localhost:8080/resolveMoviePrediction", {
+          //     method:"GET",
+          //     headers:{
+          //         "Content-Type": "application/json",
+          //     },
+          // }).then(res => {
+          //     if(!res.ok){
+          //         console.error('Request failed with status:' , res.status);
+          //         return res.text();
+          //     }
+          //     return res.json();
+          //
+          // })
+          //     .then(data => {
+          //         console.log(data);
+          //     }).catch(error=>{
+          //     console.error('Error: ', error);
+          // })
+          //
+          // ;
+          //
+          // if(data == true){
+          //     alert('Prediction made and you were so right! Go make other predictions now pookie<3');
+          //     navigate('/Home');
+          // }else{
+          //     alert('Prediction made and unfortunately you were not right. Try again pookie<3');
+          //     navigate('/Home');
+          // }
+
+
       } else {
           setError('Please select movie and date both first!');
       }
