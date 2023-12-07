cc31eda0fe60331aedc6de20c85071fcabd2a91d
Brandomon <brandon.m.lapoint@gmail.com>
2023-12-07 15:45:11 -0500

    Fixed predictionEndDate from ZonedDateTime to LocalDate. Fixed typos within printNotification method.

--- src/main/java/backend/Notifications/Notification.java
+++ src/main/java/backend/Notifications/Notification.java
@@ -2,17 +2,19 @@ package backend.Notifications;
 
 import com.google.gson.annotations.SerializedName;
 import com.google.gson.Gson;
+
+import java.time.LocalDate;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
 
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-// Notifications.Notification object class - Written by Brandon LaPointe
+// Notification object class - Written by Brandon LaPointe
 //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
 // Represents a user notification used within the Envisionary web app.
 //
 public class Notification {
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // Notifications.Notification object variables
+    // Notification object variables
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //
 	@SerializedName("notificationTitle")
@@ -25,7 +27,7 @@ public class Notification {
     private String predictionEndDate;		// Date of the prediction ending
 
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // Getter and setter methods for the Notifications.Notification object variables
+    // Getter and setter methods for the Notification object variables
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     //
     public String getNotificationTitle() {
@@ -73,21 +75,21 @@ public class Notification {
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     // printNotification
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // Method to print the Notifications.Notification object details to the console.
+    // Method to print the Notification object details to the console.
     //
     public void printNotification() {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
-        System.out.println("Notifications.Notification Title: " + notificationTitle);
-        System.out.println("BasePredictionsObject.Prediction Content: " + predictionContent);
-        System.out.println("BasePredictionsObject.Prediction Made Date: " + ZonedDateTime.parse(predictionMadeDate).toString());
-        System.out.println("BasePredictionsObject.Prediction End Date: " + ZonedDateTime.parse(predictionEndDate).toString());
+        System.out.println("Notification Title: " + notificationTitle);
+        System.out.println("Prediction Content: " + predictionContent);
+        System.out.println("Prediction Made Date: " + ZonedDateTime.parse(predictionMadeDate).toString());
+        System.out.println("Prediction End Date: " + LocalDate.parse(predictionEndDate).toString());
     }
 
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     // toJson
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // Method to convert the Notifications.Notification object to JSON.
+    // Method to convert the Notification object to JSON.
     //
     public String toJson() {
         Gson gson = new Gson();
@@ -97,7 +99,7 @@ public class Notification {
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
     // fromJson
     //■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■
-    // Static method to create a Notifications.Notification object from JSON.
+    // Static method to create a Notification object from JSON.
     //
     public static Notification fromJson(String json) {
         Gson gson = new Gson();
