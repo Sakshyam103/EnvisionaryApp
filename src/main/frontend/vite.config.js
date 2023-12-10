8d0949509d482f8ed4c4829e8021e47d12bea9cf
Sakshyam103 <ssarki@oswego.edu>
2023-12-09 19:37:06 -0500

    trying to deploy

--- src/main/frontend/vite.config.js
+++ src/main/frontend/vite.config.js
@@ -4,10 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps:{
+    main: 'src/main/frontend/src/main.jsx'
+  },
   server: {
     port: 3000
   },
    build: {
-    outDir: "public"
+    outDir: "dist"
    }
 })
